81ca01a1839553a9530a9d6e328bab00785f1c2c
adams <adamskiptalam0@gmail.com>
2025-03-18 14:06:21 +0300

    Refactoring the routes in their own component.

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import Profile from './Profile';
-import ErrorPage from './ErrorPage';
+import routes from './routes';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/profile/:name',
-    element: <Profile />,
-  }
-]);
+const router = createBrowserRouter(routes);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
