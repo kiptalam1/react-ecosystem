74e85ba517498ab8a2e07383b1112f7cf03f2eb2
adams <adamskiptalam0@gmail.com>
2025-03-18 14:00:59 +0300

    Add error-page to handle bad urls

--- src/main.jsx
+++ src/main.jsx
@@ -4,25 +4,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Profile from './Profile';
-import Spinach from './Spinach';
-import Popeye from './Popeye';
-import DefaultProfile from './DefaultProfile';
-
+import ErrorPage from './ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
-    path: '/profile',
+    path: '/profile/:name',
     element: <Profile />,
-    children: [
-      { index: true, element: <DefaultProfile />},
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye /> },
-        ]
-  },
+  }
 ]);
 
 createRoot(document.getElementById('root')).render(
