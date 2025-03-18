320d8a774df3120d2e06bc95fa4c5b7487763f84
adams <adamskiptalam0@gmail.com>
2025-03-18 13:31:14 +0300

    Create component

---
+++ src/Spinach.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";    
+
+const Spinach = () => {
+    return (
+        <>
+            <p>Hey, I am Spinach! Popeye loves to eat me!</p>
+            <Link to="/">Click here to go back</Link>
+        </>
+    );
+};
+
+export default Spinach;
